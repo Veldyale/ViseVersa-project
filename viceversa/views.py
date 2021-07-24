bd4eece27e868431daa0cbf53718a383880311b3
veldyale <veldyale@icloud.com>
2021-07-24 12:16:42 +0400

    second_commit

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html')
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    number_words = len(user_text.split(' '))
+    return render(request, 'reverse.html', {'usertext': user_text, 'numberwords' : number_words})
